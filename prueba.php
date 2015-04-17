0dcdcdd24221202b2fdd6b1436bbbebb4a2fce0b
Francisco Javier Morón <fjms@users.noreply.github.com>
2015-04-17 04:22:13 +0200

    Delete prueba.php

--- prueba.php
+++
@@ -1,8 +0,0 @@
-<?php
-require './scripts/segAdmin.php'; // Levanta session y securiza solo para admin
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-echo "Eres admin";
